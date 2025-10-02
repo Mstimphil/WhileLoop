07cc2c4710ce26f2c904803e768b82552663a684
Mstimphil <mstimphil@my.yearupunited.org>
2025-10-02 11:54:22 -0400

    Launch

--- src/main/java/com/pluralsight/For.java
+++ src/main/java/com/pluralsight/For.java
@@ -1,7 +1,13 @@
 package com.pluralsight;
 
 public class For {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 10; i > 0 ; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+
+        }
+        System.out.println("Launch");
 
     }
 }
