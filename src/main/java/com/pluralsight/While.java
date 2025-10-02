5d0563f5300e46311b7d008b6c4de09c5dd5afb6
Mstimphil <mstimphil@my.yearupunited.org>
2025-10-02 11:19:40 -0400

    initial

---
+++ src/main/java/com/pluralsight/While.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class While {
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+    }
+}
